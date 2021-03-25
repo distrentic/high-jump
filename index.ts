47099a90be8835d0446310f7f3ad414040fd539c
onatm <onatmercan@gmail.com>
2021-03-25 00:49:47 +0000

    Server client to be able to test it

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import path from "path";
 import { Server as HttpServer } from "http";
 import { Server as SocketServer } from "socket.io";
 import shell from "./shell";
@@ -8,14 +9,23 @@ const app = express();
 const server = new HttpServer(app);
 const io = new SocketServer(server, { serveClient: false, path: "/ssh" });
 
+const publicPath = path.join(__dirname, "public");
+
+app.use(express.static(publicPath));
 
 app.get("/", (req, res) => res.send("Express + TypeScript Server"));
+
+app.use('/connect', express.static(publicPath, { extensions: ['htm', 'html'] }));
 
 io.on("connection", shell);
 
 io.on("connection", (socket: any) => {
   console.log("a user connected");
 });
+
+app.get("/connect/host", (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html'));
+})
 
 server.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
