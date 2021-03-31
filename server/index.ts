976ba49fe0ac69e48367f67e94d9ec7cdf34f1c6
onatm <onatmercan@gmail.com>
2021-03-31 17:36:52 +0100

    Do not server static files

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,4 @@
 import express from "express";
-import path from "path";
 import { Server as HttpServer } from "http";
 import { Server as SocketServer } from "socket.io";
 import log from "./log";
@@ -11,22 +10,9 @@ const app = express();
 const server = new HttpServer(app);
 const io = new SocketServer(server, { serveClient: false, path: "/ssh" });
 
-const publicPath = path.join(__dirname, "public");
-
-app.use(express.static(publicPath));
-
 app.get("/", (req, res) => res.send("high-jump: Web SSH backend"));
 
-app.use(
-  "/connect",
-  express.static(publicPath, { extensions: ["htm", "html"] })
-);
-
 io.on("connection", (socket) => shell(socket, log));
-
-app.get("/connect/host", (req, res) => {
-  res.sendFile(path.join(publicPath, "index.html"));
-});
 
 server.listen(PORT, () => {
   log.info(`high-jump is running at ::${PORT}`);
